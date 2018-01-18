007843035210e31021272fa035e3d9b98828d859
QHS <quhansheng@gmail.com>
2018-01-19 02:04:42 +0800

    加入正则表达式，避免!dl!字串引发的错误

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,8 @@ import java.io.*;
 import java.net.URL;
 import java.time.Instant;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -14,29 +16,33 @@ public class Main {
         File filename = new File(beatMapFile);
         ApiManager apiManager = new ApiManager();
         String line = "";
+        Pattern pattern = Pattern.compile("http://osu.ppy.sh/b/(.*)");
         while (true) {
             BufferedReader br = new BufferedReader(new InputStreamReader(
                     new FileInputStream(filename)));
             String line2 = br.readLine();
             //StreamCompanion没开启的时候line2是null
             if(line2!=null&&!line.equals(line2)) {
-                line = line2;
-                System.out.println(Instant.now()+" 检测到谱面变化，新的谱面为："+line2+"，开始获取谱面信息");
-                Beatmap beatmap = apiManager.getBeatmap(Integer.valueOf(line.substring(20)));
-                if(beatmap!=null) {
-                    System.out.println(Instant.now()+" 开始抓取缩略图");
-                    BufferedImage image = ImageIO.read(new URL("https://b.ppy.sh/thumb/" + beatmap.getBeatmapSetId() + "l.jpg"));
-                    if(image!=null) {
-                        System.out.println(Instant.now()+" 开始写入缩略图");
-                        ImageIO.write(image, "jpg", new File(thumbFile));
-                        System.out.println(Instant.now()+" 缩略图已更新.");
-                    }else{
-                        System.out.println(Instant.now()+" 获取"+line+"的背景失败，开始重试");
+                Matcher m = pattern.matcher(line2);
+                if(m.find()) {
+                    line = line2;
+                    System.out.println(Instant.now() + " 检测到谱面变化，新的谱面为：" + line2 + "，开始获取谱面信息");
+                    Beatmap beatmap = apiManager.getBeatmap(Integer.valueOf(m.group(1)));
+                    if (beatmap != null) {
+                        System.out.println(Instant.now() + " 开始抓取"+ beatmap.getArtist() + " - " + beatmap.getTitle() + " [" + beatmap.getVersion() + "]的缩略图");
+                        BufferedImage image = ImageIO.read(new URL("https://b.ppy.sh/thumb/" + beatmap.getBeatmapSetId() + "l.jpg"));
+                        if (image != null) {
+                            System.out.println(Instant.now() + " 开始写入缩略图");
+                            ImageIO.write(image, "jpg", new File(thumbFile));
+                            System.out.println(Instant.now() + " 缩略图已更新.");
+                        } else {
+                            System.out.println(Instant.now() + " 获取" + line + "的背景失败，开始重试");
+                            line = "";
+                        }
+                    } else {
+                        System.out.println(Instant.now() + " 获取" + line + "的谱面信息失败。");
                         line = "";
                     }
-                }else{
-                    System.out.println(Instant.now()+" 获取"+line+"的谱面信息失败。");
-                    line = "";
                 }
             }
             TimeUnit.MILLISECONDS.sleep(100);
